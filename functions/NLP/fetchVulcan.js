ae8fd7f14ebd5980c1fb2a728d08693f915d12ed
jax-floyd <jaxfloyd@stanford.edu>
2023-01-01 10:48:04 -0500

    Jax's alterations to ClarosMobileApp 2023-01-01T15:48:00

--- functions/NLP/fetchVulcan.js
+++ functions/NLP/fetchVulcan.js
@@ -1,59 +1,53 @@
-const { Pool, Client } = require("pg");
-const path = require('path');
-const fs = require('fs');
-const dotenv = require('dotenv');
+import axios from 'axios';
 
-const envPath = path.join(__dirname, '..', '.env')
-const envConfig = dotenv.parse(fs.readFileSync(envPath))
-
-dotenv.config({ path: envPath, ...envConfig })
-
-const credentials = {
-  user: process.env.VULCAN_USER,
-  host: process.env.VULCAN_HOST,
-  database: process.env.VULCAN_DATABASE,
-  password: process.env.VULCAN_PASSWORD,
-  port: process.env.VULCAN_PORT,
-};
-const pool = new Pool(credentials)
-
-// Function returns promise to deliver lines, fetched from `reporter`
-// Variable inputs to allow for
-async function fetchMarket(team, bookmakers) {
-
-  let bookmakersString = ""
-  for (let i = 0; i < bookmakers.length; i++) {
-    bookmakersString += "'"
-    bookmakersString += bookmakers[i]
-    bookmakersString += "', "
+// Function returns a promise to invoke the Lambda function at the API Gateway endpoint
+export async function invokeLambda() {
+  try {
+    const response = await axios.post(`https://3jfnca8cd3.execute-api.us-east-1.amazonaws.com/default/class1Fetch`);
+    const responseData = response.data.data.message;
+    return responseData;
+    
+  } catch (error) {
+    // Handle any errors that occurred during the request here
+    console.error(error);
+    return null;
   }
-  bookmakersString = bookmakersString.slice(0, -2)
-
-  let query = "SELECT * FROM reporter WHERE (home_team_name = '" + team + "' OR away_team_name = '" + team + "') AND bookmaker IN (" + bookmakersString + ") ORDER BY max_ev DESC;";
-
-  // Connect with a client, from pool.
-  const client = await pool.connect();
-
-  // Query the events table
-  const result = await client.query(query);
-
-  // Release the client back to the connection pool
-  client.end();
-  return result.rows;
 }
 
-async function returnMarket() {
-  const bookmakers = ['bovada', 'betonlineag', 'mybookieag', 'barstool']
-  const team = 'Green Bay Packers'
-  const market = await fetchMarket(team, bookmakers);
+// Function makes good on the promise, returning relevant information from marketData object
+export async function getLambdaResponse() {
+  const marketData = await invokeLambda();
+  return marketData;
+}
+
+
+export function parseReponseObject(json) {
+  let id = json.id;
+  let eventId = json.event_id;
+  let leagueName = json.league_name;
+  let market = json.market;
+  let maxEV = json.max_ev;
+  let teamName = "";
+  let odds;
+  let point;
+  if (maxEV === json.away_ev) {
+    teamName = json.away_team_name;
+    odds = json.away_odds;
+    point = json.away_point;
+  } else if (maxEV === json.home_ev) {
+    teamName = json.home_team_name;
+    odds = json.home_odds;
+    point = json.home_point;
+  } else {
+    teamName = "Draw";
+    odds = json.draw_odds;
+  }
   
-  return market
+  if (market === "h2h") {
+    return `The ${json.away_team_name} (away) take on the ${json.home_team_name} (home) at ${json.commence_time}. ${json.bookmaker}'s ${odds} on the ${teamName} moneyline looks to be a reasonable bet.`;
+  } else if (market === "spreads") {
+    return `The ${json.away_team_name} (away) take on the ${json.home_team_name} (home) at ${json.commence_time}. ${json.bookmaker}'s ${odds} on the ${teamName} at ${point} looks to be a reasonable bet.`;
+  } else if (market === "totals") {
+    return `The ${json.away_team_name} (away) take on the ${json.home_team_name} (home) at ${json.commence_time}. ${json.bookmaker}'s ${odds} on the ${teamName} at ${point} looks to be a reasonable bet.`;
+  }
 }
-
-let market;
-// Use async / await functionality to return JSON data
-returnMarket().then(result => {
-  market = result;
-  console.log(market)
-  console.log(market.length)
-})
--- functions/NLP/generateResponse.js
+++ functions/NLP/generateResponse.js
@@ -1,70 +1,19 @@
 import axios from 'axios';
-import { containsProfanity } from "./containsProfanity"
+import { containsProfanity } from "./containsProfanity";
+import { performBinarySemanticClassification, getClassification } from "./binarySemanticClassification";
+import { invokeLambda, getLambdaResponse, parseReponseObject } from "./fetchVulcan";
 const { Configuration, OpenAIApi } = require("openai");
 
 // Function returns a promise to deliver binary classification.
-async function performBinarySemanticClassification(prompt) {
+async function formCompletionFromData(marketData) {
   try {
-    // Send a request to the FT API
-    const response = await axios.post(`https://api.openai.com/v1/engines/${'ada:ft-claros:ada-binary-semantic-classification-2022-12-31-14-54-38'}/completions`, {
-      prompt,
-      max_tokens: 2,
-      temperature: 0,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-    }, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${'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'}`
-      }
-    });
-
-    const responseText = response.data.choices[0].text;
-    return responseText.includes('1') ? 1 : 0;
-  } catch (error) {
-    // Handle any errors that occurred during the request here
-    console.error(error);
-    return null;
-  }
-}
-
-// Function makes good on the promise
-async function getClassification(prompt) {
-  const classification = await performBinarySemanticClassification(prompt);
-}
-
-// Function returns a promise to invoke the Lambda function at the API Gateway endpoint
-async function invokeLambda() {
-  try {
-    const response = await axios.post(`https://3jfnca8cd3.execute-api.us-east-1.amazonaws.com/default/class1Fetch`);
-    const responseData = response.data.data.message.home_team_name;
-    console.log(responseData)
-    return responseData;
-    
-  } catch (error) {
-    // Handle any errors that occurred during the request here
-    console.error(error);
-    return null;
-  }
-}
-
-// Function makes good on the promise, returning relevant information from marketData object
-async function getLambdaResponse() {
-  const marketData = await invokeLambda();
-  // console.log(marketData)
-}
-
-// Function returns a promise to deliver binary classification.
-async function formCompletionFromData() {
-  try {
-    // Send a request to the FT API
-    const prompt = "Construct a completion using the following data, in the following structure: The [home_team_name] are taking on the [away_team_name] at [commence_time]. [bookmaker]'s [home_odds] looks to have value, at [max_ev]% expected value.'";
-    
+    const rudimentString = parseReponseObject(marketData)
+    const prompt = `Construct a completion which conveys the same semantics in a clear, happy, respectful tone.: ${rudimentString}`;
+    console.log(prompt)
     const response = await axios.post(`https://api.openai.com/v1/engines/${'text-davinci-003'}/completions`, {
       prompt,
-      max_tokens: 1024,
-      temperature: 0,
+      max_tokens: 2048,
+      temperature: 1,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
@@ -76,6 +25,7 @@ async function formCompletionFromData() {
     });
 
     const completion = response.data.choices[0].text;
+    console.log(completion);
     return completion;
     
   } catch (error) {
@@ -86,24 +36,23 @@ async function formCompletionFromData() {
 }
 
 // Function makes good on the promise
-async function getCompletion() {
-  const completion = await formCompletionFromData();
+async function getCompletion(marketData) {
+  const completion = await formCompletionFromData(marketData);
   return completion;
 }
-
 
 // Driver function to return appropriate, user input dependent responses
 export async function generateResponse(prompt) {
   const classification = await performBinarySemanticClassification(prompt);
   if (classification === 1) {
-    const completion = await getCompletion();
+    const marketData = await getLambdaResponse();
+    const completion = await getCompletion(marketData);
     return completion;
-    return "Give me one moment to fetch realtime market data. Fetching now ..."
-
+    
   } else if (classification === 0) {
-    return "I'm a large language model trained to help with sports betting related inquires. Is there something betting related I can help with?";
+    return "I'm a artifical intillgence powered assistant trained to help with sports betting related inquires. Is there something betting related I can help with?";
   
   } else {
-    return "Sorry, there was an error getting the classification. Please try again later.";
+    return "Sorry, there was an error getting the classification. Please try again later or contact info@claros.ai for support.";
   }
 }
